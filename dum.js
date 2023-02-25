9f70acc154939aeb0d0a5fca8231c46701733776
YOUNG-K <30793281+quamepossible@users.noreply.github.com>
2023-02-24 19:47:23 -0500

    Just a dummy test

--- dum.js
+++ dum.js
@@ -1,14 +1,3 @@
 'use strict';
 
-const mainData = {
-    model: "6",
-    version: "s",
-    imei: "944872856530972"
-}
-const subData = {
-    data: "2023-02-19",
-    version: 'sp'
-}
-
-const newData = Object.assign({}, mainData, subData);
-console.log(newData);
+'Hello world'
